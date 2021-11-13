cabfca3c765c2ca0645c352993b1b119219a4cb3
Justin Chase <justin.m.chase@gmail.com>
2021-11-13 15:43:13 -0600

    Run -d flag

--- commands/run.ts
+++ commands/run.ts
@@ -3,22 +3,26 @@ import { getScripts } from "./util.ts";
 
 interface IArgs {
   name: string;
+  opts?: Record<string, unknown>;
   args?: string[];
 }
 
 export const run = new Command()
   .stopEarly()
+  .option("-d, --directory <directory:string>", "The working directory to use when running the script.")
   .arguments("<script:string> [...args]")
   .description("Runs a script")
-  .action(async (_opts, script, args) => {
-    await runCommand({ name: script, args });
+  .action(async (opts, script, args) => {
+    await runCommand({ name: script, opts, args });
   });
 
-async function runScript(name: string, args: string[]): Promise<number> {
+async function runScript(name: string, opts: Record<string, unknown>, args: string[]): Promise<number> {
   const cwd = await Deno.realPath(Deno.cwd());
   const scriptsInstallDir = path.join(cwd, ".deno");
   const scriptsBinDir = path.join(scriptsInstallDir, "bin");
-  const installProcess = Deno.run({
+  const runDir = opts.directory ? opts.directory as string : cwd
+  const runProcess = Deno.run({
+    cwd: runDir,
     env: {
       ...Deno.env.toObject(),
       PATH: [scriptsBinDir, Deno.env.get("PATH")].join(
@@ -32,20 +36,20 @@ async function runScript(name: string, args: string[]): Promise<number> {
     stdout: "inherit",
     stderr: "inherit",
   });
-  const { code } = await installProcess.status();
+  const { code } = await runProcess.status();
   return code;
 }
 
-async function runScripts(scripts: string[]): Promise<number> {
+async function runScripts(scripts: string[], opts: Record<string, unknown>): Promise<number> {
   for (const script of scripts) {
-    const code = await runScript(script, []);
+    const code = await runScript(script, opts, []);
     if (code !== 0) return code;
   }
 
   return 0;
 }
 
-async function runCommand({ name, args = [] }: IArgs) {
+async function runCommand({ name, opts = {}, args = [] }: IArgs) {
   const scripts = await getScripts();
   if (!scripts) return;
 
@@ -58,8 +62,8 @@ async function runCommand({ name, args = [] }: IArgs) {
   }
 
   const code = Array.isArray(script)
-    ? await runScripts(script)
-    : await runScript(name, args);
+    ? await runScripts(script, opts)
+    : await runScript(name, opts, args);
 
   Deno.exit(code);
 }
